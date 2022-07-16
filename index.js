1d49fa8ec50f0fc82798620ff6dfa6275ff4563c
Сергей Фомин <xaoc615@gmail.com>
2022-07-16 19:33:13 +0300

    litteJS

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-/*hello
-world*/
-//beeee
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Change this line
+// Only change code above this line
