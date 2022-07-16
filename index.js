3d7ee229a2a5099d6d117f2ba0b8ea1607e50292
Сергей Фомин <xaoc615@gmail.com>
2022-07-16 19:34:47 +0300

    arrJS

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const myNoun = "dog";
-const myAdjective = "big";
-const myVerb = "ran";
-const myAdverb = "quickly";
-
-// Only change code below this line
-var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Change this line
-// Only change code above this line
+const myArray = ['littleJS', 2];
