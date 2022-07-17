8c73c05b992c29a3d3f33f78b06522fe13748e10
Сергей Фомин <xaoc615@gmail.com>
2022-07-17 14:31:01 +0300

    objects

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
-let count = 0;
-
-function cc(card) {
-  // Only change code below this line
-switch (card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    case 10:
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-      count--;
-      break;
+function phoneticLookup(val) {
+    var result = "";
+    var lookup = {
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank"
+    };
+  // After converting our case statements into object properties you can make use of the variable `result` to let the function return the correct value.
+  
+  
+    result = lookup[val];
+    // Only change code above this line
+    return result;
   }
-  if (count > 0) {
-    return count + " Bet";
-  } else {
-    return count + " Hold";
-  }
-
-  return "Change Me";
-  // Only change code above this line
-}
-
-cc(2); cc(3); cc(7); cc('K'); cc('A');
