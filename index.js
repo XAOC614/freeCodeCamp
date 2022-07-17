44d96aba0452e54d73e81466793f654a263d7404
Сергей Фомин <xaoc615@gmail.com>
2022-07-17 11:30:17 +0300

    function

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const myList = [
-    ["Chocolate Bar", 15],
-    ["Camel", 3],
-    ["Cola", 4],
-    ["fanta", 5],
-    ["Sprite", 5],
-    ["Water", 2]
-  ];
+function reusableFunction() {
+    console.log("Hi World");
+  }
+  reusableFunction();
+  
