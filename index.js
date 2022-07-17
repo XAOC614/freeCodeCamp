45782508702487150b78f95db7ff46ff92cb099c
Сергей Фомин <xaoc615@gmail.com>
2022-07-17 12:54:31 +0300

    switch

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
-
-function golfScore(par, strokes) {
-  // Only change code below this line
-if (strokes == 1) {
-    return names[0];
-  } else if (strokes <= par - 2) {
-    return names[1];
-  } else if (strokes === par - 1) {
-    return names[2];
-  } else if (strokes === par) {
-    return names[3];
-  } else if (strokes === par + 1) {
-    return names[4];
-  } else if (strokes === par + 2) {
-    return names[5];
-  } else {
-    return names[6];
+function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+  switch(val){
+    case "a":
+    answer = "apple";
+    break;
+    case "b":
+    answer = "bird";
+    break;
+    case "c":
+    answer = "cat";
+    break;
+    default:
+    answer = "stuff";
+    break
   }
-
   
-  // Only change code above this line
-}
-
-golfScore(5, 4);
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  switchOfStuff(1);
