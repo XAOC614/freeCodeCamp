eabf59b29438761a6ff8890421eeda51313dc387
Сергей Фомин <xaoc615@gmail.com>
2022-07-17 12:38:26 +0300

    largeOrTiny

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-function testElseIf(val) {
-    if (val > 10) {
-      return "Greater than 10";
-    }
+function testSize(num) {
+    // Only change code below this line
+  if (num<5){
+    return "Tiny"
+  }else if (num<10){
+    return "Small"
+  }else if(num<15){
+    return "Medium"
+  }else if(num<20){
+    return "Large"
+  }else if(num>=20){
+    return "Huge"
+  }else
   
-    else if (val < 5) {
-      return "Smaller than 5";
-    }
-  else {
-    return "Between 5 and 10";
-  }
+    return "Change Me";
+    // Only change code above this line
   }
   
-  testElseIf(7);
+  testSize(7);
