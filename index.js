2a44afbbc21cc92657e31fd2d287b44ac0a5c84d
Сергей Фомин <xaoc615@gmail.com>
2022-07-17 11:54:23 +0300

    func

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// Setup
-let sum = 0;
-
-function addThree() {
-  sum = sum + 3;
-}
-
-// Only change code below this line
-function addFive() {
-  sum = sum + 5;
-}
-
-// Only change code above this line
-
-addThree();
-addFive();
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    const removed = arr.shift();
+    return removed;
+    // Only change code above this line
+  }
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
