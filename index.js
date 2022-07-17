852e6f4b64cfbd8c8a3bedf4a86f3f20008b89ab
Сергей Фомин <xaoc615@gmail.com>
2022-07-17 13:55:17 +0300

    casino

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-function chainToSwitch(val) {
-    let answer = "";
-    // Only change code below this line
-  
-    switch (val){
-      case "bob":
-      answer = "Marley";
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
       break;
-      case 42:
-      answer = "The Answer";
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
       break;
-      case 1:
-      answer = "There is no #1";
-      break;
-      case 99:
-      answer = "Missed me by this much!";
-      break;
-      case 7:
-      answer = "Ate Nine";
-      break;
-    }
-    // Only change code above this line
-    return answer;
   }
-  
-  chainToSwitch(7);
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
