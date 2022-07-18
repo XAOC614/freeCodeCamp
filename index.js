91ddab6e7d2f4be386a38909099d35b860d0134d
Сергей Фомин <xaoc615@gmail.com>
2022-07-18 15:49:42 +0300

    recursion

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-// Setup
-const myArray = [];
-for (let i=1; i<6; i++){
-  myArray.push(i)
-}
-// Only change code below this line
+function sum(arr, n) {
+    // Only change code below this line
+  if (n <= 0) {
+        return 0;
+      } else {
+        return sum(arr, n-1) + arr[n-1];
+      }
+    }
+    // Only change code above this line
+  
